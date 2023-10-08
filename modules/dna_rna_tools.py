eb7e9b656462675e2fc20d99510e246291fb8be4
Stacy <stacy.petukhova@gmail.com>
2023-10-08 15:04:59 +0300

    Add transcribe function

--- modules/dna_rna_tools.py
+++ modules/dna_rna_tools.py
@@ -21,3 +21,19 @@ RNA_DICT = {
 }
 DNA_BASES = "ACGTacgt"
 RNA_BASES = "ACGUacgu"
+
+
+def transcribe(seq: str) -> str:
+    """
+    Transcribes DNA sequence into RNA
+
+    Arguments:
+    - seq (str): DNA sequence
+
+    Returns:
+    - output (str): RNA sequence
+    """
+    if "U" in seq or "u" in seq:
+        raise ValueError("Can't transcribe RNA!")
+    else:
+        return seq.replace("T", "U").replace("t", "u")
