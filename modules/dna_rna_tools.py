f7a164194fb63a22653fa5ce66fbb3dcb3ca3baa
asyapetukhova <144522017+asyapetukhova@users.noreply.github.com>
2023-10-08 15:44:17 +0300

    Update dna_rna_tools.py

--- modules/dna_rna_tools.py
+++ modules/dna_rna_tools.py
@@ -1,28 +1,3 @@
-# Constants
-DNA_DICT = {
-    "A": "T",
-    "T": "A",
-    "C": "G",
-    "G": "C",
-    "a": "t",
-    "t": "a",
-    "c": "g",
-    "g": "c",
-}
-RNA_DICT = {
-    "A": "U",
-    "U": "A",
-    "C": "G",
-    "G": "C",
-    "a": "u",
-    "u": "a",
-    "c": "g",
-    "g": "c",
-}
-DNA_BASES = "ACGTacgt"
-RNA_BASES = "ACGUacgu"
-
-
 def transcribe(seq: str) -> str:
     """
     Transcribes DNA sequence into RNA
