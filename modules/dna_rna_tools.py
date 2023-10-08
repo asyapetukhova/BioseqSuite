3708e4250db9e58a90b2f70e1da3c270e6c09e64
Stacy <stacy.petukhova@gmail.com>
2023-10-08 15:02:03 +0300

    Add constants in dna_rna_tools.py

---
+++ modules/dna_rna_tools.py
@@ -0,0 +1,23 @@
+# Constants
+DNA_DICT = {
+    "A": "T",
+    "T": "A",
+    "C": "G",
+    "G": "C",
+    "a": "t",
+    "t": "a",
+    "c": "g",
+    "g": "c",
+}
+RNA_DICT = {
+    "A": "U",
+    "U": "A",
+    "C": "G",
+    "G": "C",
+    "a": "u",
+    "u": "a",
+    "c": "g",
+    "g": "c",
+}
+DNA_BASES = "ACGTacgt"
+RNA_BASES = "ACGUacgu"
