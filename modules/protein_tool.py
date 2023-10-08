577dd184a5455d1454fac2c7b39b776b6e77ac77
asyapetukhova <144522017+asyapetukhova@users.noreply.github.com>
2023-10-08 16:03:44 +0300

    Update protein_tool.py

--- modules/protein_tool.py
+++ modules/protein_tool.py
@@ -1,61 +1,3 @@
-def classify_amino_acid(seq: str) -> str:
-    """
-    Determine the percentage of acidic, basic and neutral amino acids in protein.
-
-    Arguments:
-    - seq (str): amino acid sequence. The input must be uppercased and use the single letter amino acid code.
-
-    Returns
-    - output (str): percentage of neutral, acidic and basic amino acids in protein.
-    """
-    amino_acid_counts = {'acidic': 0, 'neutral': 0, 'basic': 0}
-    aa_classification = {'acidic': ['D', 'E'], 'neutral': ['A', 'N', 'C', 'Q', 'G', 'I', 'L', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'],
-                         'basic': ['H','R', 'K']}
-    for i in seq:
-        key = [key for key, value in aa_classification.items() if i in value]
-        amino_acid_counts[key[0]] += 1
-    acidic_percentage = round(amino_acid_counts['acidic'] / len(seq) * 100, 2)
-    neutral_percentage = round(amino_acid_counts['neutral'] / len(seq) * 100, 2)
-    basic_percentage = round(amino_acid_counts['basic'] / len(seq) * 100, 2)
-    output = f'neutral: {neutral_percentage}, acidic: {acidic_percentage}, basic: {basic_percentage}'
-    return output
-
-
-def counting_point_mutations(seq1: str, seq2: str) -> int:
-    """
-    Counts the number of mutations - amino acid substitutions in the sequence seq2 relative to seq1.
-    Input sequences must have the same length.
-
-    Arguments:
-    - seq1 (str): sequence to compare with
-    - seq2 (str): sequence to compare to
-
-    Return:
-    - output (int): number of amino acid substitutions
-    """
-    output = 0
-    for number_amino_acid in range(len(seq1)):
-        if seq1[number_amino_acid] != seq2[number_amino_acid]:
-            output += 1
-    return output
-
-
-def counting_molecular_weight(seq: str) -> int:
-    """
-    Counts the molecular mass of a protein sequence seq
-
-    Arguments:
-    - seq (str): sequence to count the molecular weight
-
-    Return:
-    - output (int): molecular weight value
-    """
-    output = 0
-    for amino_acid in seq:
-        output += DICT_MOLECULAR_MASS[amino_acid]
-    return output - 18 * (len(seq) - 1)
-
-
 def get_occurrences(seq1: str, seq2: str) -> str:
     """
    Counting the number of occurrences of string seq2 in string seq1.
@@ -95,22 +37,6 @@ def find_amino_acid_indices(seq: str, amino_acid: str) -> str:
         if aa == amino_acid:
             indices.append(index + 1)
     output = ', '.join(str(i) for i in indices)
-    return output
-
-
-def count_variant_rna(seq: str) -> int:
-    """
-    Сounts the number of RNAs that can be a template for the synthesis of the entered sequence
-
-    Arguments:
-    - seq (str): amino acid sequence. The input must be uppercased and use the single letter amino acid code
-
-    Returns:
-    - output (int): number of RNAs that can be a template for the synthesis of the entered sequence
-    """
-    output = 1
-    for i in seq:
-        output = output * NUMBER_CODONS[i]
     return output
 
 
