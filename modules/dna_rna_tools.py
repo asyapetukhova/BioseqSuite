92c1c676572bd2907924ce6f7f087694f0af5b75
Stacy <stacy.petukhova@gmail.com>
2023-10-08 15:06:35 +0300

    Add complement function

--- modules/dna_rna_tools.py
+++ modules/dna_rna_tools.py
@@ -50,3 +50,19 @@ def reverse(seq: str) -> str:
     - output (str): reversed sequence
     """
     return seq[::-1]
+
+
+def complement(seq: str) -> str:
+    """
+    Returns the complementary sequence
+
+    Arguments:
+    - seq (str): DNA or RNA sequence
+
+    Returns:
+    - output (str): complementary sequence
+    """
+    if "U" in seq or "u" in seq:
+        return "".join([RNA_DICT[base] for base in seq])
+    else:
+        return "".join([DNA_DICT[base] for base in seq])
