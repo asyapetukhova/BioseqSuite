b5a72d3f34c0c5f7a3e00cfcd33fca96908d6511
asyapetukhova <144522017+asyapetukhova@users.noreply.github.com>
2023-10-08 15:44:50 +0300

    Update BioseqSuite.py

--- BioseqSuite.py
+++ BioseqSuite.py
@@ -1,3 +1,28 @@
+# Constants
+DNA_DICT = {
+    "A": "T",
+    "T": "A",
+    "C": "G",
+    "G": "C",
+    "a": "t",
+    "t": "a",
+    "c": "g",
+    "g": "c",
+}
+RNA_DICT = {
+    "A": "U",
+    "U": "A",
+    "C": "G",
+    "G": "C",
+    "a": "u",
+    "u": "a",
+    "c": "g",
+    "g": "c",
+}
+DNA_BASES = "ACGTacgt"
+RNA_BASES = "ACGUacgu"
+
+
 def protein_tool(*args: str) -> str:
     """
     Main function that is used to get sequence(s) and command. It performs a given action with the entered sequence
