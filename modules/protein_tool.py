0e4105292352ad2ffebb27764b97f1ed00faae21
asyapetukhova <144522017+asyapetukhova@users.noreply.github.com>
2023-10-08 16:04:53 +0300

    Update protein_tool.py

--- modules/protein_tool.py
+++ modules/protein_tool.py
@@ -1,24 +1,3 @@
-def get_occurrences(seq1: str, seq2: str) -> str:
-    """
-   Counting the number of occurrences of string seq2 in string seq1.
-   Getting indexes of occurrences of string seq2 in string seq1.
-
-    Arguments:
-    - seq1 (str): sequence in which search
-    - seq2 (str): sequence to search in
-
-    Return:
-    - output (str): str, first element is the number of occurrences (int).
-      All subsequent elements are indexes of occurrences of seq2 in seq1.
-    """
-    output = [seq1.count(seq2)]
-    for i in range(len(seq1)):
-        if seq1.startswith(seq2, i):
-            output.append(i + 1)
-    return (f'Number of occurrences: {output[0]}; '
-            f'indexes: {", ".join(str(element) for element in output[1:])}')
-
-
 def find_amino_acid_indices(seq: str, amino_acid: str) -> str:
     """
     Finds the amino acid indices specified in the input.
