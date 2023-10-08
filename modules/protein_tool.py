420ad4ad28f776b0bb252a7766cc85818080e0c6
Stacy <stacy.petukhova@gmail.com>
2023-10-08 15:15:29 +0300

    Add get_occurrences function

--- modules/protein_tool.py
+++ modules/protein_tool.py
@@ -79,3 +79,24 @@ def counting_molecular_weight(seq: str) -> int:
     for amino_acid in seq:
         output += DICT_MOLECULAR_MASS[amino_acid]
     return output - 18 * (len(seq) - 1)
+
+
+def get_occurrences(seq1: str, seq2: str) -> str:
+    """
+   Counting the number of occurrences of string seq2 in string seq1.
+   Getting indexes of occurrences of string seq2 in string seq1.
+
+    Arguments:
+    - seq1 (str): sequence in which search
+    - seq2 (str): sequence to search in
+
+    Return:
+    - output (str): str, first element is the number of occurrences (int).
+      All subsequent elements are indexes of occurrences of seq2 in seq1.
+    """
+    output = [seq1.count(seq2)]
+    for i in range(len(seq1)):
+        if seq1.startswith(seq2, i):
+            output.append(i + 1)
+    return (f'Number of occurrences: {output[0]}; '
+            f'indexes: {", ".join(str(element) for element in output[1:])}')
