c9fed5c5e99aed816bd6e761781718c61b5fd2db
asyapetukhova <144522017+asyapetukhova@users.noreply.github.com>
2023-10-08 16:20:20 +0300

    Import modules

--- BioseqSuite.py
+++ BioseqSuite.py
@@ -1,5 +1,13 @@
 from collections import Counter
 from modules.calculate_protein import calculate_amino_acid_percentages, calculate_pI
+from modules.classify_amino_acid import classify_amino_acid
+from modules.counting_protein import counting_point_mutations, counting_molecular_weight, count_variant_rna
+from modules.find_amino_acid_indices import find_amino_acid_indices
+from modules.get_occurrences import get_occurrences
+from modules.is_protein import is_protein
+from modules.reverse_and_complement import reverse, complement, reverse_complement
+from modules.total_protein_charge import determine_total_protein_charge
+from modules.transcribe import transcribe
 import sys
 
 # Constants
