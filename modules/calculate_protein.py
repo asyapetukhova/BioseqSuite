fb6f0da185c33713b6af48cc64ee967b0d22524f
asyapetukhova <144522017+asyapetukhova@users.noreply.github.com>
2023-10-20 17:05:36 +0300

    Add is_protein function

--- modules/calculate_protein.py
+++ modules/calculate_protein.py
@@ -156,3 +156,17 @@ def find_amino_acid_indices(seq: str, amino_acid: str) -> str:
             indices.append(index + 1)
     output = ', '.join(str(i) for i in indices)
     return output
+
+
+def is_protein(seq: str, aa_set = set('FLIMVSPTAYHQNKDECWRG')) -> bool:
+    """
+    Check whether the transmitted sequence consists of amino acids
+
+    Arguments:
+    - seq (str): amino acid sequence. The input must be uppercased and use the single letter amino acid code
+    - aa_set(set): all amino acid that we use. May be replaced with extra amino acids or it's modifications. But other functions aren't not intended to work with unusual amino acids
+
+    Returns:
+    - bool: True if the sequence is an amino acid sequence, False if it contains other symbols
+    """
+    return True if set(seq) <= AA_SET else False
