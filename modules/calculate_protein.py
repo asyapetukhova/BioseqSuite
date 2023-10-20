07e9dcf02488042197ff6f2b1e7145da79ffeb40
asyapetukhova <144522017+asyapetukhova@users.noreply.github.com>
2023-10-20 17:06:57 +0300

    Add determine_total_protein_charge function

--- modules/calculate_protein.py
+++ modules/calculate_protein.py
@@ -170,3 +170,22 @@ def is_protein(seq: str, aa_set = set('FLIMVSPTAYHQNKDECWRG')) -> bool:
     - bool: True if the sequence is an amino acid sequence, False if it contains other symbols
     """
     return True if set(seq) <= AA_SET else False
+
+
+def determine_total_protein_charge(seq) -> str:
+    """
+    Determine whether the protein has positive, negative or neutral charge in neutral pH
+
+    Arguments:
+    - seq (str): amino acid sequence. The input must be uppercased and use the single letter amino acid code
+
+    Returns:
+    - output (str): positive, negative or neutral charge of protein in neutral pH
+    """
+    seq_list = list(seq.strip())
+    aa_cnt = Counter(seq_list)
+    number_of_pos = sum([aa_cnt[aa] for aa in POS_CHARGED])
+    number_of_neg = sum([aa_cnt[aa] for aa in NEG_CHARGED])
+    if number_of_pos == number_of_neg:
+        return 'neutral'
+    return 'positive' if number_of_pos > number_of_neg else 'negative'
