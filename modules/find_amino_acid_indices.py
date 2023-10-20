85f1142240c435a112de09074e6688e37688c189
asyapetukhova <144522017+asyapetukhova@users.noreply.github.com>
2023-10-20 17:02:38 +0300

    Update find_amino_acid_indices.py

--- modules/find_amino_acid_indices.py
+++ modules/find_amino_acid_indices.py
@@ -1,19 +1,1 @@
-def find_amino_acid_indices(seq: str, amino_acid: str) -> str:
-    """
-    Finds the amino acid indices specified in the input.
 
-    Arguments:
-    - seq (str): amino acid sequence. The input must be uppercased and use the single letter amino acid code.
-    - amino_acid (str): amino acid for which indices need to be found.
-
-    Returns:
-    - output (str): all found indices in the protein for which the entered amino acid corresponds to.
-    """
-    indices = []
-    if amino_acid not in seq:
-        raise ValueError('Amino acid not found')
-    for index, aa in enumerate(seq):
-        if aa == amino_acid:
-            indices.append(index + 1)
-    output = ', '.join(str(i) for i in indices)
-    return output
