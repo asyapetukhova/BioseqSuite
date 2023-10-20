c69c7d60a2eb767cbdac903c0b031e0772eb698a
asyapetukhova <144522017+asyapetukhova@users.noreply.github.com>
2023-10-20 22:16:18 +0300

    Add constants

--- modules/reverse_and_complement.py
+++ modules/reverse_and_complement.py
@@ -1,3 +1,28 @@
+# Constants
+DNA_DICT = {
+    "A": "T",
+    "T": "A",
+    "C": "G",
+    "G": "C",
+    "a": "t",
+    "t": "a",
+    "c": "g",
+    "g": "c",
+}
+RNA_DICT = {
+    "A": "U",
+    "U": "A",
+    "C": "G",
+    "G": "C",
+    "a": "u",
+    "u": "a",
+    "c": "g",
+    "g": "c",
+}
+DNA_BASES = "ACGTacgt"
+RNA_BASES = "ACGUacgu"
+
+
 def reverse(seq: str) -> str:
     """
     Reverses the sequence
