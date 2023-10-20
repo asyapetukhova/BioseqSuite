0a731224f5a31f79ebe9490646433241353c37f5
asyapetukhova <144522017+asyapetukhova@users.noreply.github.com>
2023-10-20 22:18:31 +0300

    Add transcribe function

--- modules/reverse_and_complement.py
+++ modules/reverse_and_complement.py
@@ -63,3 +63,19 @@ def reverse_complement(seq: str) -> str:
     - output (str): Reverse complement sequence
     """
     return complement(reverse(seq))
+
+
+def transcribe(seq: str) -> str:
+    """
+    Transcribes DNA sequence into RNA
+
+    Arguments:
+    - seq (str): DNA sequence
+
+    Returns:
+    - output (str): RNA sequence
+    """
+    if "U" in seq or "u" in seq:
+        raise ValueError("Can't transcribe RNA!")
+    else:
+        return seq.replace("T", "U").replace("t", "u")
